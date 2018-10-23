2b72b9eb2a4ebe17a6be47254c665eda0fc697f1
pioka <okapy142@gmail.com>
2018-10-24 01:56:47 +0900

    [Add] indexページを実装

--- board/models.py
+++ board/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
+    archived = models.BooleanField()
 
     def __str__(self):
         return self.title
--- board/views.py
+++ board/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from .models import Article
+
+
 # Create your views here.
+def index(request):
+    article_list = Article.objects.filter(archived=False).order_by('-id')
+
+    context = {
+        'page': 'Home',
+        'article_list': article_list
+    }
+    return render(request, 'board/index.html', context)
+
+
+def archivedArticles(request):
+    article_list = Article.objects.filter(archived=True).order_by('-id')
+
+    context = {
+        'page': 'Archived',
+        'article_list': article_list
+    }
+    return render(request, 'board/index.html', context)
