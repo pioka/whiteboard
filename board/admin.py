514a46489ef21716b732755217d699abd9d27a30
pioka <p10mirror2@gmail.com>
2018-11-20 18:59:00 +0900

    :floppy_disk: DBにボードとタグを追加

--- board/admin.py
+++ board/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
+from .models import Board
+from .models import Tag
 from .models import Article
 
 
 # Register your models here.
+admin.site.register(Board)
+admin.site.register(Tag)
 admin.site.register(Article)
--- board/models.py
+++ board/models.py
@@ -1,11 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Article(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
-    archived = models.BooleanField()
+class Board(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    members = models.ManyToManyField(User , through='Membership', related_name='members')
 
     def __str__(self):
-        return self.title
+        return self.name
+
+
+class Membership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+
+class Tag(models.Model):
+    COLORS = (
+        ('PR', 'Primary'),
+        ('SU', 'Success'),
+        ('DA', 'Danger'),
+        ('WA', 'Warning'),
+        ('IN', 'Info'),
+        ('SE', 'Secondary'))
+
+    name = models.CharField(max_length=16)
+    color = models.CharField(max_length=2, choices=COLORS)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    subject = models.CharField(max_length=255)
+    body = models.TextField()
+    is_archived = models.BooleanField()
+    updated_at = models.DateTimeField()
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    label = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subject
